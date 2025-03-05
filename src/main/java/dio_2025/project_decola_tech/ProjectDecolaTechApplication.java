44399d87bde7088dbd0d0cd5544e5da95d2ae4ce
Eduardo Takashi Missaka <10417877@mackenzista.com.br>
2025-03-05 15:57:05 -0300

    Initial commit

---
+++ src/main/java/dio_2025/project_decola_tech/ProjectDecolaTechApplication.java
@@ -0,0 +1,13 @@
+package dio_2025.project_decola_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDecolaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDecolaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio_2025/project_decola_tech/ProjectDecolaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package dio_2025.project_decola_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDecolaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
