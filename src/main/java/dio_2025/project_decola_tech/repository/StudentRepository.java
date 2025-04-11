347e8b810dd1c614997af8652d71a95a6185aa6a
Eduardo Takashi Missaka <10417877@mackenzista.com.br>
2025-04-11 14:10:43 -0300

    Update StudentRepository.java

--- src/main/java/dio_2025/project_decola_tech/repository/StudentRepository.java
+++ src/main/java/dio_2025/project_decola_tech/repository/StudentRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-  boolean existsByRegistrationNumber(String registrationNumber);
+  boolean existsByRegistrationNumber(String number);
 }
